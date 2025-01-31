cb435b7ba9928e9cce8efb377b0a53554f9bab61
Rafael Catimbang <rafaelangelo.catimbang@unc.edu.ph>
2025-01-31 15:20:04 +0800

    removed unnecessary inheritance

--- Dealer.java
+++ Dealer.java
@@ -2,18 +2,12 @@ package BlackJack2;
 
 public class Dealer extends Person {
 
-    // Dealer must draw to 16 and stand on 17
-    @Override
     public void drawTo17() {
         while (getScore(hand) < 17) drawCard(false);
     }
+
     public void displayFirstCard() {
         System.out.println("Dealer's Hand: " + hand.getFirst() + " X \n");
-    }
-
-    @Override
-    public int decideAction() {
-        return 0;
     }
 
     public boolean hasAce() {
